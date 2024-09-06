b35260204f8fbf1c1884eb1695bb78d15cc64fc8
TylerJac <sans.gaming235@gmail.com>
2024-09-06 14:47:52 -0400

    sample 1

---
+++ src/main/java/org/rbm/ResTfulBookManagementApplication.java
@@ -0,0 +1,13 @@
+package org.rbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulBookManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulBookManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rbm/ResTfulBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulBookManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
